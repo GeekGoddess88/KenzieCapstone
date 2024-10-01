a8205765e2a9f3c0b412b0001860854da0093bf8
John Olson <69521422+olsonjohn@users.noreply.github.com>
2024-09-30 22:24:24 -0500

    Revert "merging main into nikki's branch"

--- Application/src/main/java/com/kenzie/appserver/repositories/DrinkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kenzie.appserver.repositories;
-
-import com.kenzie.appserver.repositories.model.DrinkRecord;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.springframework.data.repository.CrudRepository;
-
-@EnableScan
-public interface DrinkRepository extends CrudRepository<DrinkRecord, String> {
-}
